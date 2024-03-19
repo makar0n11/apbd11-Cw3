a1802a619f6d60b4a6b4787499c1d3cfbf0554b9
makaron11 <makaron1711@gmail.com>
2024-03-19 14:25:32 +0100

    init

--- cw3/cw3/Program.cs
+++ cw3/cw3/Program.cs
@@ -1,11 +1,3 @@
-﻿namespace cw3
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            
-        }
-    }
-    
-}
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
